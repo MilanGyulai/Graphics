6e950070a97659081d969de8e75fd42110ead07a
BoBByMilan <milan.gyulai@student.uni-miskolc.hu>
2026-02-16 17:34:39 +0100

    feat: Matrix_transformations folder feat: made matrix.c, main.c, makefile, matrix.h

--- demos/Matrix_transformations/main.c
+++ demos/Matrix_transformations/main.c
@@ -1,13 +1,13 @@
 #include "matrix.h"
 
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[])
+{
 
     float A[3][3] = {
         {1.0, 2.0, 3.0},
         {4.0, 5.0, 6.0},
-        {7.0, 8.0, 9.0}
-    };
-    
+        {7.0, 8.0, 9.0}};
+
     float B[3][3];
 
     initialMatrix(B);
@@ -16,4 +16,3 @@ int main(int argc, char* argv[]){
 
     return 0;
 }
-
--- demos/Matrix_transformations/matrix.h
+++ demos/Matrix_transformations/matrix.h
@@ -7,6 +7,6 @@ void printMatrix(float matrix[3][3]);
 
 void multiplyMatrixes(float matrix[3][3], float matrixB[3][3], float matrixC[3][3]);
 
-void matrix_x_Vectors(float number1,float number2, float vector[3], float matrix[3][3]);
+void matrix_x_Vectors(float number1, float number2, float vector[3], float matrix[3][3]);
 
 #endif
